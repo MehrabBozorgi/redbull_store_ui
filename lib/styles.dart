54e7faa19d26f10e5f395d3c4202152beaf24334
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-04-20 07:33:15 +0430

    redbull_store_ui

---
+++ lib/styles.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+const drink1Color = Color(0xFF17319F);
+const greyColor = Color(0xFFDEE1E4);
+const drink2Color = Color(0xFF0E81B7);
+const drink3Color = Color(0xFFD53867);
+const buttonColor = Color(0xFFfb0244);
+
+const title1Style = TextStyle(
+    color: Colors.white,
+    fontSize: 18,
+    fontWeight: FontWeight.normal,
+    letterSpacing: 1);
+//
+const title2Style = TextStyle(
+  color: Colors.white,
+  fontSize: 32,
+  fontWeight: FontWeight.bold,
+);//
+const title2Style2 = TextStyle(
+  color: Colors.white,
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+//
+const titleBlackStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 28,
+  fontWeight: FontWeight.bold,
+);
+//
+const descStyle = TextStyle(
+  color: Color(0xFF424242),
+  fontSize: 14,
+  fontWeight: FontWeight.bold,
+  wordSpacing: 1,
+height: 1.5
+);
+//
+const catStyle = TextStyle(
+  color: Colors.black,
+  fontSize: 14,
+  fontWeight: FontWeight.bold,
+);
+//
+const cat1Style = TextStyle(
+  color: Colors.white,
+  fontSize: 14,
+  fontWeight: FontWeight.bold,
+);
+//
+const smallTitleStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 12,
+  fontWeight: FontWeight.normal,
+);
