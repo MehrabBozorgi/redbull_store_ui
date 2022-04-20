a20959d5fb5307c1fb5b1fe5c20d5fac718a07bb
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-04-20 07:32:03 +0430

    redbull_store_ui

---
+++ lib/model.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ModelClass {
+  final String id;
+  final String title1;
+  final String title2;
+  final String title3;
+  final String image;
+  final String price;
+  final Color color;
+
+  ModelClass({
+    required this.id,
+    required this.title1,
+    required this.title2,
+    required this.title3,
+    required this.image,
+    required this.price,
+    required this.color,
+  });
+}
